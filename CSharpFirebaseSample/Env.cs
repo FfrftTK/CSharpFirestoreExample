93c3030cfac2921fdc5ae884ef354b2be8a83273
FfrftTK <t1610415@uec.ac.jp>
2020-03-09 18:06:40 +0900

    first commit

---
+++ CSharpFirebaseSample/Env.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+using System.Runtime.CompilerServices;
+
+namespace CSharpFirebaseSample
+{
+    public static class Env
+    {
+        /// <summary>
+        /// Name of the file to manage environment variables.
+        /// Change if needed.
+        /// </summary>
+        public const string ENV_FILE_NAME = ".env";
+        static readonly string workingDirectory = Environment.CurrentDirectory;
+        static readonly string projectDirectory = Directory.GetParent(workingDirectory).Parent.Parent.FullName;
+
+        public static void Load()
+        {
+            DotNetEnv.Env.Load(Path.Combine(projectDirectory, ENV_FILE_NAME));
+        }
+
+        public static string GetString([CallerMemberName] string key = "") => DotNetEnv.Env.GetString(key);
+        public static int GetInt([CallerMemberName] string key = "") => DotNetEnv.Env.GetInt(key);
+        public static bool GetBool([CallerMemberName] string key = "") => DotNetEnv.Env.GetBool(key);
+        public static double GetDouble([CallerMemberName] string key = "") => DotNetEnv.Env.GetDouble(key);
+
+        public static string SERVICE_ACCOUNT_JSON_PATH => GetString();
+        public static string PROJECT_ID => GetString();
+    }
+}
---
+++ CSharpFirebaseSample/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using Google.Cloud.Firestore;
+
+namespace CSharpFirebaseSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Env.Load();
+            var ProjectId = Env.PROJECT_ID;
+            Environment.SetEnvironmentVariable(
+                "GOOGLE_APPLICATION_CREDENTIALS",
+                Path.Combine(AppContext.BaseDirectory, Env.SERVICE_ACCOUNT_JSON_PATH));
+            AddDataExample(ProjectId).Wait();
+        }
+
+        /// Add data to DB
+        private static async Task AddDataExample(string project)
+        {
+            FirestoreDb db = FirestoreDb.Create(project);
+            // [START fs_add_data_1]
+            DocumentReference docRef = db.Collection("users").Document("test");
+            Dictionary<string, object> user = new Dictionary<string, object>
+            {
+                { "First", "Ada" },
+                { "Last", "Lovelace" },
+                { "Born", 1815 }
+            };
+            await docRef.SetAsync(user);
+            // [END fs_add_data_1]
+            Console.WriteLine("Added data to the alovelace document in the users collection.");
+        }
+    }
+}
